0e623d4c9ace1d1d560c15f526cb3b9773861988
una <una@ubivelox.com>
2018-04-13 17:46:43 +0900

    add login

---
+++ blogupdater/blogupdater/View/RepoCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepoCellTableViewCell.swift
+//  blogupdater
+//
+//  Created by una on 2018. 4. 13..
+//  Copyright © 2018년 una. All rights reserved.
+//
+
+import UIKit
+
+class RepoCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
