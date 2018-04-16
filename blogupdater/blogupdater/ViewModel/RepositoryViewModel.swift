8bde24cad58f6f813bc3a28f70bec42f4178232f
una <una@ubivelox.com>
2018-04-16 14:33:45 +0900

    ling repository

---
+++ blogupdater/blogupdater/ViewModel/RepositoryViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RepositoryViewModel.swift
+//  blogupdater
+//
+//  Created by una on 2018. 4. 16..
+//  Copyright © 2018년 una. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryViewModel: Any {
+
+}
