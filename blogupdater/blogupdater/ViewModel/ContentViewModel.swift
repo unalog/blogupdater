e4eadccb6a459ef526e14930b47526833289603f
una <una@ubivelox.com>
2018-04-17 11:13:39 +0900

    add content view controller

---
+++ blogupdater/blogupdater/ViewModel/ContentViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentViewModel.swift
+//  blogupdater
+//
+//  Created by una on 2018. 4. 17..
+//  Copyright © 2018년 una. All rights reserved.
+//
+
+import UIKit
+
+class ContentViewModel: Any {
+
+}
