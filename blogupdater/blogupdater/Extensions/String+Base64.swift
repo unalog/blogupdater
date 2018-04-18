f8e8cb30d0a6ca864d375741e7e2a6fbd083e543
una <una@ubivelox.com>
2018-04-18 15:30:39 +0900

    add makrdownEditVC

---
+++ blogupdater/blogupdater/Extensions/String+Base64.swift
@@ -0,0 +1,31 @@
+//
+//  String+addition.swift
+//  blogupdater
+//
+//  Created by una on 2018. 4. 18..
+//  Copyright © 2018년 una. All rights reserved.
+//
+
+import Foundation
+
+extension String{
+    
+    func fromBase64()->String?  {
+        
+        guard let data = Data(base64Encoded: self) else { return nil }
+        return String(data: data, encoding: .utf8)
+    }
+    
+    func toBase64() -> String? {
+        
+        guard let data = self.data(using: String.Encoding.utf8) else {
+            return nil
+        }
+        
+        return data.base64EncodedString(options: Data.Base64EncodingOptions(rawValue: 0))
+    }
+
+    static func toBase64File() -> String {
+        
+    }
+}
