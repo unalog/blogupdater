b688fe9ddde2c79d4bac014ad0e1cb6e65eeaa0e
una <una@ubivelox.com>
2018-04-20 13:57:46 +0900

    add create file

---
+++ blogupdater/blogupdater/Extensions/UIViewController_Rx.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController_Rx.swift
+//  blogupdater
+//
+//  Created by una on 2018. 4. 20..
+//  Copyright © 2018년 una. All rights reserved.
+//
+
+import Foundation
---
+++ blogupdater/blogupdater/Model/EditContentResult.swift
@@ -0,0 +1,9 @@
+//
+//  EditFileResult.swift
+//  blogupdater
+//
+//  Created by una on 2018. 4. 19..
+//  Copyright © 2018년 una. All rights reserved.
+//
+
+import Foundation
